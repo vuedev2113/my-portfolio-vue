d89e0ae119586f1ffdc66b17d0c45d2dfae2c633
vuedev2113 <eduarmora251@gmail.com>
2024-10-12 08:51:24 +0900

    completion of projects section in main page
    Co-authored-by: creative2113 <scottking2113@gmail.com>

--- src/components/HomePage/ProjectCard.vue
+++ src/components/HomePage/ProjectCard.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>
-    <h2>{{ project.name }}</h2>
-    <p>{{ project.description }}</p>
-    <h3>Role: {{ project.role }}</h3>
-    <v-img alt="project Image" :src="project.image" />
-    <ul>
-      <li v-for="tool in project.tools" :key="tool">{{ tool }}</li>
-    </ul>
+  <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full">
+    <div className="flex flex-row">
+      <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600" />
+      <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent" />
+    </div>
+    <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
+      <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
+        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400" />
+        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400" />
+        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200" />
+      </div>
+      <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
+        {{ project.name }}
+      </p>
+    </div>
+    <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+      <code className="font-mono text-xs md:text-sm lg:text-base">
+        <div className="blink">
+          <span className="mr-2 text-pink-500">const</span>
+          <span className="mr-2 text-white">project</span>
+          <span className="mr-2 text-pink-500">=</span>
+          <span className="text-gray-400">{{ '{' }}</span>
+        </div>
+        <div>
+          <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+          <span className="text-gray-400">{{ `'` }}</span>
+          <span className="text-amber-300">{{ project.name }}</span>
+          <span className="text-gray-400">{{ `',` }}</span>
+        </div>
+
+        <div className="ml-4 lg:ml-8 mr-2">
+          <span className=" text-white">tools:</span>
+          <span className="text-gray-400">{{ ` ['` }}</span>
+          <span v-for="tag, i in project.tools" :key="i">
+            <span className="text-amber-300">{{ tag }}</span>
+            <span v-if="project.tools.length - 1 !== i" className="text-gray-400">{{ `', '` }}</span>
+          </span>
+          <span className="text-gray-400">{{ "]," }}</span>
+        </div>
+        <div>
+          <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
+          <span className="text-orange-400">{{ project.role }}</span>
+          <span className="text-gray-400">,</span>
+        </div>
+        <div className="ml-4 lg:ml-8 mr-2">
+          <span className="text-white">Description:</span>
+          <span className="text-cyan-400">{{ ' ' + project.description }}</span>
+          <span className="text-gray-400">,</span>
+        </div>
+        <div><span className="text-gray-400">{{ `};` }}</span></div>
+      </code>
+    </div>
   </div>
 </template>
 
--- src/components/HomePage/Projects.vue
+++ src/components/HomePage/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="projects" class="relative z-50  my-12 lg:my-24">
+  <div id="projects" class="relative w-full z-50  my-12 lg:my-24">
     <div class="sticky top-10">
       <div class="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30" />
       <div class="flex items-center justify-start relative">
@@ -11,8 +11,8 @@
     </div>
 
     <div class="pt-24">
-      <div class="flex flex-col gap-6">
-        <div v-for="project, index in projectsData" :key="index" class="sticky-card w-full mx-auto max-w-2xl sticky">
+      <div class="flex items-center flex-col gap-6">
+        <div v-for="project, index in projectsData" :key="index" class="sticky-card w-full mx-auto max-w-2xl sticky top-20">
           <div class="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
             <ProjectCard :project="project" />
           </div>
